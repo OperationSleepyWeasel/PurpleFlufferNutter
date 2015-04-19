adbc2b526c34f253fa729e45f45951cc9b11e4a2
qiubix <qiubix@gmail.com>
2015-04-19 15:31:56 +0200

    Switch view to list screen on button click

--- app/src/main/java/sleepyweasel/purplefluffernutter/MainActivity.java
+++ app/src/main/java/sleepyweasel/purplefluffernutter/MainActivity.java
@@ -1,9 +1,12 @@
 package sleepyweasel.purplefluffernutter;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,15 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button listButton = (Button) findViewById(R.id.listButton);
+        listButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent listView = new Intent(getApplicationContext(), MovieListActivity.class);
+                startActivity(listView);
+            }
+        });
     }
 
 
