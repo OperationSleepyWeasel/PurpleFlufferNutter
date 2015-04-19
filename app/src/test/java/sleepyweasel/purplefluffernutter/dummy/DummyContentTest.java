442fec683c2fb213b658a2ffca4707d202302737
tempaowca <artus.sawicki@gmail.com>
2015-04-19 13:39:59 +0200

    adding Mockito

--- app/src/test/java/sleepyweasel/purplefluffernutter/dummy/DummyContentTest.java
+++ app/src/test/java/sleepyweasel/purplefluffernutter/dummy/DummyContentTest.java
@@ -4,8 +4,12 @@ import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class DummyContentTest {
+
+    private static final String RETURN_STRING = "tralalala";
 
     @Test
     public void shouldRun() {
@@ -20,5 +24,14 @@ public class DummyContentTest {
     @Test
     public void shouldRunWithAssertJ() {
         assertThat(true).isTrue();
+    }
+
+    @Test
+    public void shouldUseMock() {
+        DummyContent dummyContent = mock(DummyContent.class);
+
+        when(dummyContent.toString()).thenReturn(RETURN_STRING);
+
+        assertThat(dummyContent.toString()).isEqualTo(RETURN_STRING);
     }
 }
