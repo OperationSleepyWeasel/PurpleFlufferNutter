1c50f51dae6297a5e4e17315b2257bde7b81cc4c
tempaowca <artus.sawicki@gmail.com>
2015-04-19 12:00:02 +0200

    adding dummy test with junit

---
+++ app/src/test/java/sleepyweasel/purplefluffernutter/dummy/DummyContentTest.java
@@ -0,0 +1,18 @@
+package sleepyweasel.purplefluffernutter.dummy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DummyContentTest {
+
+    @Test
+    public void shouldRun() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void shouldRun2() {
+        assertFalse(false);
+    }
+}
