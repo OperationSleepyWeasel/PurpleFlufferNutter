db0599a3202072046ac6b4a04168e309a76c02c7
qiubix <qiubix@gmail.com>
2015-04-19 15:58:16 +0200

    Add summary activity

--- app/src/main/java/sleepyweasel/purplefluffernutter/SummaryActivity.java
+++ app/src/main/java/sleepyweasel/purplefluffernutter/SummaryActivity.java
@@ -1,35 +1,24 @@
 package sleepyweasel.purplefluffernutter;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class SummaryActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button listButton = (Button) findViewById(R.id.listButton);
-        listButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent listView = new Intent(getApplicationContext(), MovieListActivity.class);
-                startActivity(listView);
-            }
-        });
+        setContentView(R.layout.activity_summary);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_summary, menu);
         return true;
     }
 
@@ -46,15 +35,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void goToSummary(View v) {
-        Intent i = new Intent(getApplicationContext(), SummaryActivity.class);
-        startActivity(i);
-    }
-
-    public void goToSettings(View v) {
-        Intent i = new Intent(getApplicationContext(), SettingsActivity.class);
-        startActivity(i);
     }
 }
