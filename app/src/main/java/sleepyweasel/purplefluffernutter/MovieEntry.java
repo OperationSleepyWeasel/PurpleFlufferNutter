d3f8b2e8f9d46fac96fc0378aac377bbaf3d6375
qiubix <qiubix@gmail.com>
2015-05-15 13:04:25 +0200

    Lombok constructor for MovieEntry

--- app/src/main/java/sleepyweasel/purplefluffernutter/MovieEntry.java
+++ app/src/main/java/sleepyweasel/purplefluffernutter/MovieEntry.java
@@ -6,12 +6,8 @@ import lombok.ToString;
 
 @Getter
 @ToString
-//@AllArgsConstructor
-public class MovieEntry {
+@AllArgsConstructor(suppressConstructorProperties = true)
+class MovieEntry {
 
     private String title;
-
-    MovieEntry(String title) {
-        this.title = title;
-    }
 }
