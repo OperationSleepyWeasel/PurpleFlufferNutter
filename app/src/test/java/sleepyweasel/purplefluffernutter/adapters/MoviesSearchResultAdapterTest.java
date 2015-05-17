cec97c47e4a138720aac3fd7badcf3438f5e7ecb
tempaowca <artus.sawicki@gmail.com>
2015-05-17 12:11:24 +0200

    adding custom adapter test class with empty tests

---
+++ app/src/test/java/sleepyweasel/purplefluffernutter/adapters/MoviesSearchResultAdapterTest.java
@@ -0,0 +1,24 @@
+package sleepyweasel.purplefluffernutter.adapters;
+
+import org.junit.Test;
+
+//FIXME: write these tests, i do not know how to.
+public class MoviesSearchResultAdapterTest {
+
+    private MoviesSearchResultAdapter adapter;
+
+    @Test
+    public void shouldSetTitleTextView() {
+
+    }
+
+    @Test
+    public void shouldSetReleaseDateTextView() {
+
+    }
+
+    @Test
+    public void shouldHandleNUllReleaseDate() {
+
+    }
+}
