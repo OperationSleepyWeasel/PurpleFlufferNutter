73e65181b0908098a6a4b8cb4261b88d4bd418e5
tempaowca <artus.sawicki@gmail.com>
2015-05-17 16:23:06 +0200

    adding configuration endpoint

--- app/src/main/java/sleepyweasel/purplefluffernutter/rest/tmdb/Tmdb.java
+++ app/src/main/java/sleepyweasel/purplefluffernutter/rest/tmdb/Tmdb.java
@@ -2,9 +2,13 @@ package sleepyweasel.purplefluffernutter.rest.tmdb;
 
 import retrofit.http.GET;
 import retrofit.http.Query;
+import sleepyweasel.purplefluffernutter.rest.tmdb.domain.Configuration;
 import sleepyweasel.purplefluffernutter.rest.tmdb.domain.SearchResult;
 
 public interface Tmdb {
     @GET("/search/movie")
     SearchResult searchMovie(@Query("query") String query);
+
+    @GET("/configuration")
+    Configuration getConfiguration();
 }
